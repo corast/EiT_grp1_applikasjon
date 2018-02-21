b79ed07e77b9e44e15a0594f37c510bf12ae1845
Bennaren <36695845+Bennaren@users.noreply.github.com>
2018-02-21 14:20:15 +0100

    AstroEnvironment

---
+++ AstroEnvironment/Assets/OrbitAround.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+    public GameObject Sun;
+    public float speed=0.001;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        OrbitAround();
+    }
+    void OrbitAround()
+    {
+        transform.RotateAround(Sun.transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
+}
---
+++ AstroEnvironment/Assets/Scripts/Ellipse.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Ellipse : MonoBehaviour
+{
+
+   public LineRenderer lineRenderer;
+
+    //Segments in our ellipse
+    [Range(3, 36)]
+    public int segments;
+    public float xAxis = 5f;
+    public float yAxis = 3f;
+    Vector3 SunPosition = new Vector3(-40, 0, 30);
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        lineRenderer.SetColors()
+        lineRenderer = GetComponent<LineRenderer>();
+        CalculateEllipseSun();
+    }
+
+    void CalculateEllipseSun()
+    {
+        Vector3[] points = new Vector3[segments + 1];
+        for (int i = 0; i < segments; i++)
+        {
+            float angle = ((float)i / (float)segments) * 360 * Mathf.Deg2Rad;
+            float x = Mathf.Cos(angle) * xAxis;
+            float y = Mathf.Sin(angle) * yAxis;
+            points[i] = new Vector3(x,y, 0f) + SunPosition;
+        }
+        points[segments] = points[0];
+
+        lineRenderer.positionCount = segments + 1;
+        lineRenderer.SetPositions(points);
+    }
+
+   
+    /// <summary>
+    /// Called when the script is loaded or a value is changed in the
+    /// inspector (Called in the editor only).
+    /// </summary>
+    void OnValidate()
+    {
+        CalculateEllipseSun();
+     
+    }
+}
