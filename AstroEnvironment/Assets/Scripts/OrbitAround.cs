2d0762862a3646a0d854fd5ff15477c6b22334a1
Bennaren <36695845+Bennaren@users.noreply.github.com>
2018-02-22 14:50:07 +0100

    Scripts + visuelle forbedringer

---
+++ AstroEnvironment/Assets/Scripts/OrbitAround.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitAround : MonoBehaviour {
+
+    public GameObject Midpoint;
+    public float speed= 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Orbit();
+    }
+    void Orbit()
+    {
+        transform.RotateAround(Midpoint.transform.position, Vector3.back, speed * Time.deltaTime);
+    }
+}
+
---
+++ AstroEnvironment/Assets/Scripts/OwnRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwnRotation : MonoBehaviour
+{
+
+    public GameObject ObjectCoordinates;
+    public float speed = 10f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+    }
+    void Rotate()
+    {
+        transform.RotateAround(ObjectCoordinates.transform.position, Vector3.back, speed * Time.deltaTime);
+    }
+}
+
