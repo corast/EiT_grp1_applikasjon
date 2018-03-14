9e2d7c7f576619827b556118edcdd5c053ff1038
corast <corastweb94@hotmail.com>
2018-03-14 14:13:33 +0100

    Start menu version 1

---
+++ AstroEnvironment/Assets/StartMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+	public void startGame(){
+		//Change scene by one.
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void quitGame(){
+		Debug.Log("Exiting Application");
+		Application.Quit();
+	}
+}
