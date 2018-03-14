c711fe1a86562c3f755dfb4f36a7972b2e0723b2
Filip Schjerven <filipschjerven@dhcp-10-24-44-87.wlan.ntnu.no>
2018-03-14 09:38:58 +0100

    Added partial script for greenhouseeffect and photon sine wave

---
+++ AstroEnvironment/Assets/Scripts/GreenhouseGasEffect.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenhouseGasEffect : MonoBehaviour {
+
+	public float opacity;
+	public float size;
+	public Color colorGasSphere;
+
+	private GameObject gasSphere;
+	private float planetSize;
+	private Color fromColor;
+	private Color toColor;
+	private float t;
+
+	// Use this for initialization
+	void Start () {
+		//Create and place a sphere
+		gasSphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+		gasSphere.transform.localScale = transform.localScale*1.1f;
+
+		var matGasSphere = gasSphere.GetComponent<Renderer>().material;
+		//I have no clue what this does, but it's essential for it to work
+		matGasSphere.SetFloat ("_Mode", 3);
+		matGasSphere.SetInt ("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+		matGasSphere.SetInt ("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+		matGasSphere.SetInt ("_ZWrite", 0);
+		matGasSphere.DisableKeyword ("_ALPHATEST_ON");
+		matGasSphere.DisableKeyword ("_ALPHABLEND_ON");
+		matGasSphere.EnableKeyword ("_ALPHAPREMULTIPLY_ON");
+		matGasSphere.renderQueue = 3000;
+
+		fromColor = new Color (0.5f, 0.5f, 0.5f, 0.05f);
+		toColor = new Color (0.5f, 0.5f, 0.5f, 0.75f);
+		t = -10f;
+		gasSphere.GetComponent<Renderer> ().material.color = fromColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Ensure the sphere is positioned around the planet
+		gasSphere.transform.position = transform.position;
+		if (0 <= t && t < 1f) {
+			var lerpedColor = Color.Lerp (fromColor, toColor, t);
+			gasSphere.GetComponent<Renderer> ().material.color = lerpedColor;
+			t += (1f / 300f);
+		} else if (t < 0) {
+			t += 1f / 100f;
+		}
+		print (t);
+
+
+		//if raycast, change size and transparency?
+		//gasSphere.renderer.material.color.a = 0.5;
+	}
+}
--- AstroEnvironment/Assets/Scripts/PlanetCameraOrientator.cs
+++ AstroEnvironment/Assets/Scripts/PlanetCameraOrientator.cs
@@ -15,7 +15,8 @@ public class PlanetCameraOrientator : MonoBehaviour {
 	private GameObject pointCameraFocus;
 
 	private Vector3 centreSun;
-	private float t;
+	private float zt;
+	private float rt;
 	private Vector3 ab;
 	private Vector3 bc;
 
@@ -24,10 +25,15 @@ public class PlanetCameraOrientator : MonoBehaviour {
 		return Mathf.Sqrt( Mathf.Pow (distFrom.x - distTo.x, 2) + Mathf.Pow (distFrom.y - distTo.y, 2) + Mathf.Pow (distFrom.z - distTo.z, 2));
 	}
 
+	//
 	float GetAngle (Vector3 line1, Vector3 line2, bool rad = true) {
 		line1.Normalize ();
 		line2.Normalize ();
 		float angle = Mathf.Acos (line1.x * line2.x + line1.z * line2.z);
+		//Checking sign of angle
+		if (line1.z < line2.z) {
+			angle = -angle;
+		}
 		if (rad)
 			return angle;
 		else
@@ -43,7 +49,7 @@ public class PlanetCameraOrientator : MonoBehaviour {
 		//Position if earth was in "perfect alignment" to hardcoded values
 		var idealPosition = centreSun + new Vector3 (Distance (centreSun, planet.transform.position), 0 ,0);
 
-		//Points in which to zoom the camera from, via, by and have the camera focus at
+		//Points in which to the camera moves  via, to and have its focus on
 		pointTo = new GameObject ("pointTo");
 		pointTo.transform.position = idealPosition + new Vector3 (-sizePlanet * 0.625f, sizePlanet*0.125f, sizePlanet * 0.25f);
 
@@ -57,33 +63,41 @@ public class PlanetCameraOrientator : MonoBehaviour {
 		Vector3 lineSunPlanet = planet.transform.position - centreSun;
 		lineSunPlanet.Normalize ();
 		float angleToIdeal = GetAngle (Vector3.right, lineSunPlanet, false);
-		if (planet.transform.position.z > idealPosition.z) {
-			angleToIdeal = -angleToIdeal;
-		}
-		print (angleToIdeal);
 
-		//Rotate the hardcoded points so they correspond to earth
+		//Rotate the hardcoded points so they correspond to earths actual position
 		pointTo.transform.RotateAround (centreSun, Vector3.up, angleToIdeal);
 		pointVia.transform.RotateAround (centreSun, Vector3.up, angleToIdeal);
 		pointCameraFocus.transform.RotateAround (centreSun, Vector3.up, angleToIdeal);
-		t = 0;
+		zt = 0;
+		rt = 0;
 	}
 	
 	void LateUpdate () {
 
 		//Updating points so they rotate along with earth
-		pointTo.transform.RotateAround(centreSun,  Vector3.up, speedOrbit * Time.deltaTime);
-		pointVia.transform.RotateAround(centreSun,  Vector3.up, speedOrbit * Time.deltaTime);
-		pointCameraFocus.transform.RotateAround(centreSun,  Vector3.up, speedOrbit * Time.deltaTime);
+		pointTo.transform.RotateAround (centreSun,  Vector3.up, speedOrbit * Time.deltaTime);
+		pointVia.transform.RotateAround (centreSun,  Vector3.up, speedOrbit * Time.deltaTime);
+		pointCameraFocus.transform.RotateAround (centreSun,  Vector3.up, speedOrbit * Time.deltaTime);
 
-		//Should be replaced with raycast, and t = 0 after if it's to repeated
-		if (t < 1)  {
-			ab = Vector3.Lerp (transform.position, pointVia.transform.position, t);
-			bc = Vector3.Lerp (pointVia.transform.position, pointTo.transform.position, t);
-			transform.position = Vector3.Lerp (ab, bc, t);
+		//Nice rotation from start of zoom
+		if (rt < 1) {
+			var targetRotation = Quaternion.LookRotation(pointCameraFocus.transform.position - transform.position);
+
+			// Smoothly rotate towards the target point.
+			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rt);
+			rt += speedAnimation * (1.25f - Mathf.Pow (rt, 2));
+		}
+
+		//Movement of the camera to its destination point
+		//Should be replaced with raycast, and t = 0 after, if it's to repeated
+		if (zt < 1 && rt >= 0.9)  {
+			ab = Vector3.Lerp (transform.position, pointVia.transform.position, zt);
+			bc = Vector3.Lerp (pointVia.transform.position, pointTo.transform.position, zt);
+			transform.position = Vector3.Lerp (ab, bc, zt);
 			transform.LookAt (pointCameraFocus.transform.position);
-			t += speedAnimation * (1.25f - Mathf.Pow (t, 2));
-		} else if (t >= 1) {
+			zt += speedAnimation * (1.25f - Mathf.Pow (zt, 2));
+		}
+		if (zt >= 1) {
 			transform.position = pointTo.transform.position;
 			transform.LookAt (pointCameraFocus.transform.position);
 		}
