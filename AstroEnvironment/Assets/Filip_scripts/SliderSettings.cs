b7048814f0a6b746eabee63aa80738ae72f28799
corast <corastweb94@hotmail.com>
2018-04-11 10:38:03 +0200

    Adding slider

---
+++ AstroEnvironment/Assets/Filip_scripts/SliderSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderSettings : MonoBehaviour {
+
+	public Slider mainSlider;
+
+	public void OnSliderChange(){
+		
+		float value = mainSlider.value;
+		//Gjør et elelr annet med infoen fra slideren.
+		Debug.Log(value);
+
+	}
+}
