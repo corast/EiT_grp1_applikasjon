98d795996a7363cded3053b5c907045194f70cfd
corast <corastweb94@hotmail.com>
2018-04-11 12:22:58 +0200

    Added fading script(_GM)

---
+++ AstroEnvironment/Assets/Scripts/Fading.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fading : MonoBehaviour {
+
+	public Texture2D fadeOutTexture; //Black image
+	public float fadeSpeed = 0.1f;
+
+	private int drawDepth = -1000;
+
+	private float alpha = 1.0f;
+	private float fadeDir = -1; // in = -1, 1= out.
+
+	/// <summary>
+	/// This function is called when the object becomes enabled and active.
+	/// </summary>
+	void OnEnable()
+	{
+		SceneManager.sceneLoaded += OnLevelFinishedLoading;
+	}
+	/// <summary>
+	/// This function is called when the behaviour becomes disabled or inactive.
+	/// </summary>
+	void OnDisable()
+	{
+		SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+	}
+
+	/// <summary>
+	/// OnGUI is called for rendering and handling GUI events.
+	/// This function can be called multiple times per frame (one call per event).
+	/// </summary>
+	void OnGUI()
+	{
+		//fade in/out
+		alpha += fadeDir * fadeSpeed * Time.deltaTime;
+		// force (clamp) the number to be between 0 and 1 because GUI.color uses Alpha values between 0 and 1
+		alpha = Mathf.Clamp01(alpha);
+
+		// set color of our GUI (in this case our texture). All color values remain the same & the Alpha is set to the alpha variable
+		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+		GUI.depth = drawDepth;                // make the black texture render on top (drawn last)
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);  // draw the texture to fit the entire screen area
+ 	}
+
+	// sets fadeDir to the direction parameter making the scene fade in if -1 and out if 1
+	public float BeginFade (int direction)
+	{	
+		Debug.Log("BeginFade");
+		fadeDir = direction;
+		return (fadeSpeed);
+	}
+
+	// OnLevelWasLoaded is called when a level is loaded. It takes loaded level index (int) as a parameter so you can limit the fade in to certain scenes.
+	void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+	{
+		// alpha = 1;  // use this if the alpha is not set to 1 by default
+		BeginFade(-1);  // call the fade in function
+	}
+	//OnLevelFinishedLoading
+}
--- AstroEnvironment/Assets/Scripts/StartMenu.cs
+++ AstroEnvironment/Assets/Scripts/StartMenu.cs
@@ -5,9 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour {
 
+	//public GameObject _GM; 
+
 	public void startGame(){
+		float fadeTime = GameObject.Find("_GM").GetComponent<Fading>().BeginFade(1);
+		delay(fadeTime);
 		//Change scene by one.
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	private IEnumerable delay(float time){
+		yield return new WaitForSeconds(time);
 	}
 
 	public void quitGame(){
