35fa5c632db9a8e0becf014f360d963956e78277
corast <corastweb94@hotmail.com>
2018-04-11 11:14:54 +0200

    Added more sliders

--- AstroEnvironment/Assets/Filip_scripts/SliderSettings.cs
+++ AstroEnvironment/Assets/Filip_scripts/SliderSettings.cs
@@ -5,11 +5,29 @@ using UnityEngine.UI;
 
 public class SliderSettings : MonoBehaviour {
 
-	public Slider mainSlider;
+	public Slider slider1;
+	public Slider slider2;
+	public Slider slider3;
 
-	public void OnSliderChange(){
+	public void OnSliderChangeOne(){
 		
-		float value = mainSlider.value;
+		float value = slider1.value;
+		//Gjør et elelr annet med infoen fra slideren.
+		Debug.Log(value);
+
+	}
+	
+	public void OnSliderChangeTwo(){
+		
+		float value = slider2.value;
+		//Gjør et elelr annet med infoen fra slideren.
+		Debug.Log(value);
+
+	}
+	
+	public void OnSliderChangeThree(){
+		
+		float value = slider3.value;
 		//Gjør et elelr annet med infoen fra slideren.
 		Debug.Log(value);
 
